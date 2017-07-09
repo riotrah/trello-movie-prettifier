85311b9097d4cccde6a0209b61a7dd63bc5be91a
riotrah <rmr478@nyu.edu>
2017-07-09 17:07:32 -0400

    Implemented Trello interfacing and everything else really

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # trello-movie-prettifier
 This lil node app converts simple plaintext Trello cards with movie titles as their card titles into aesthetic cards containing links and details of each including a poster thumbnail!
+
+# Overview
+The app will crawl a given board for cards of a broadly defined format. It grabs data based on the title of the card, from The Movie DataBase.
+It then updates that card with its proper title, a poster, genre labels, its synopsis and its TMDB rating!
+
+## Note:
+Its not very intelligent, so it grabs the *first* movie it gets results from.
+
+If any word in the movie name is mispelled, it will fail for that card!
+
+# In action
+
+# How to run
+
+1. Grab a TMDB Application Key.
+2. Grab a Trello API Key.
+3. Grab the associated API Token.
+4. Grab the id of your desired trello board by appending '.json' to its url and looking for the value of the key 'id' in the first object that appears.
+5. Clone this repo.
+6. Create an `.env` file in the directory root  and place the following text in it, replacing the parts that need to be replaced.
+
+	TMBD_KEY='<your TMDB key here'
+	T_KEY='<your Trello key here'
+	T_TOKEN='<your Trello token here'
+	T_BOARD='<your Trello board id here>'
+
+7. Fill your board with cards with the titles of movies. The closer the name to the actual title of the movie, the better. 
+
+"Space Odyssey" will correct to "2001: A Space Odyssey" and will work
+"Space Oddysy" will be skipped
+"The Blair Witch Project" will work, and will populate the card with information for the 1999 movie
+"Blair Witch" will work, but will populate the card with information for the recent remake/sequel - so beware what you type!
+
+
+# Current issues:
+
+[ ] - It may make too many requests at once due to its async nature 
+[ ] - It fails on slight misspellings of words
+
+# Credits
+
+[The Movie DataBase API](https://www.themoviedb.org/documentation/api) "This product uses the TMDb API but is not endorsed or certified by TMDb." 
--- app.js
+++ app.js
@@ -1,3 +1,151 @@
-// Connect to MovieDB api via MovieDB library
-const movie = require('./movieGrabber.js');
-const trello = require('/trello.js');
+// ----------------------------------------------------------------------------
+// 
+// trello-movie-prettifier
+// The Trello movie data grabber you never knew you wanted
+// 
+// Made with a passion for movies, posters and scripts
+// - Rayat Rahman
+// - gh: riotrah
+// 
+// License: http://opensource.org/licenses/MIT
+// 
+// ----------------------------------------------------------------------------
+
+require('dotenv').config();
+const Trello = require('node-trello');
+const t = new Trello(process.env.T_KEY, process.env.T_TOKEN);
+const movie = require('./moviegrabber.js');
+
+// Grab the board to update from env
+const boardId = process.env.T_BOARD;
+t.get('/1/boards/'+boardId+'/cards', function(err, data) {
+  // console.log(data);
+  movie.setup()
+  .then(()=>{
+    handleCards(data);
+  });
+});
+
+/**
+ * Runs the loop to process the array of cards
+ * @param  {Array} cards The array of Card objects returned by Trello's request
+ */
+function handleCards(cards) {
+
+  cards.forEach((card) => {
+    if(!isPretty(card)) {
+      console.log('Grabbing data for', card.name);
+      grabMovieFromCard(card);
+    } else {
+      console.log('Already prettified', card.name);
+    }
+  });
+}
+
+/**
+ * Checks if a card is already prettified
+ * Currently simply checks if the year was added to the card
+ * @param  {Object}  card Trello API Card object
+ * @return {Boolean}      Is it pretty?
+ */
+function isPretty(card) {
+
+  dateRegEx = /\(\b(19|20)\d{2}\b\)/g;
+  if(card.name.substr(card.name.length - 6).match(dateRegEx)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * Grabs card and passes it to movieGrabber.js
+ * Then takes that data and updates card with it
+ * @param  {Object} card Trello API card object
+ */
+function grabMovieFromCard(card) {
+
+  movie.grab(card.name)
+  .then((movie) => {
+    addMovieDetails(card, movie);
+  }).catch((err) => {
+    console.log(err);
+  });
+}
+/**
+ * Applies data from movie grab to a given card
+ * @param  {Object} card  Trello API card object
+ * @param  {Object} movie movieGrabber.js Movie Object
+ */
+function addMovieDetails(card, movie) {
+
+  const cardUrl = '/1/cards/'+card.id;
+  const cardDetails = {
+    name: movie.name,
+    desc: movie.desc,
+  };
+
+  t.put(cardUrl, cardDetails, (err, res) => {
+    if(err) {
+      console.log(err);
+    } else {
+      // console.log(res);
+      addPoster(card, movie);
+    }
+  });
+}
+
+/**
+ * Adds movie poster image url as attachment to card
+ * @param  {Object} card  Trello API card object
+ * @param  {Object} movie movieGrabber.js Movie Object
+ */
+function addPoster(card, movie) {
+
+  const cardAttachUrl = '/1/cards/'+card.id+'/attachments';
+  const attachment = {
+    url: movie.attachment,
+  };
+
+  t.post(cardAttachUrl, attachment, (err, res) => {
+    if(err) {
+      console.log(err);
+    } else {
+      // console.log(res);
+      addGenres(card, movie);
+    }
+  });
+}
+
+/**
+ * Adds movie genres as labels to card
+ * @param  {Object} card  Trello API card object
+ * @param  {Object} movie movieGrabber.js Movie Object
+ */
+function addGenres(card, movie) {
+
+  const cardLabelsUrl = '/1/cards/'+card.id+'/labels';
+
+  movie.labels.forEach((l)=>{
+    const label = {
+      name: l,
+    };
+    t.post(cardLabelsUrl, label, (err, res) => {
+      if(err) {
+        console.log(err);
+      } else {
+        // console.log(res);
+        handleCardSuccess(movie);
+      }
+    });
+  });
+}
+
+/**
+ * Handles success of updating an individual card
+ * @param  {Object} movie movieGrabber.js Movie Object
+ */
+function handleCardSuccess(movie) {
+
+  console.log('Successfully grabbed!', movie.name);
+}
