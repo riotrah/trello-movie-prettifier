b25cb28eecfb82dede38da6f65533494ded13291
riotrah <rmr478@nyu.edu>
2017-07-09 17:04:55 -0400

    Added .env reading and linting dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "google",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "rules": {
+    	"padded-blocks" : 0,
+    	"keyword-spacing" : 0
+    }
+};
